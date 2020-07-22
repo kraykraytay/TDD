4ef24f95b9af6ac069038f8194c17ef671a94692
kraykraytay <68570365+kraykraytay@users.noreply.github.com>
2020-07-22 16:25:29 +0100

    Update name_function.py

--- name_function.py
+++ name_function.py
@@ -1,6 +1,6 @@
 def formatted_name(last_name, first_name, middle_name=''):
     if len(middle_name) > 0:
-        full_name = last_name + ", " + first_name + " " + middle_name
+        full_name = last_name + ", " + first_name + " " + middle_name # printing names in a format used for school i.e alphatebical order of surnames 
     else:
         full_name = last_name + ", " + first_name
     return full_name.title()
